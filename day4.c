585d7efaa287e1cbd49641537507494491874f34
chris.lee <chris.lee@chamberlain.com>
2022-12-08 16:47:55 -0600

    working on part 1

--- day4.c
+++ day4.c
@@ -20,8 +20,91 @@ bool elf_range_contains(elf_range a, elf_range b) {
     }
 }
 
-bool pair_has_contain(char *line) {
+void get_pair_from_line(char *line, elf_range *a, elf_range *b) {
     int alow, blow, ahigh, bhigh;
 
+    char *buffer;
+
     int first_dash, comma, second_dash;
+
+    for (int i = 0; i < strlen(line); i++) { // getting first dash allows us to get alow
+        if (line[i] == '-') {
+            first_dash = i;
+            buffer = (char*)calloc(i, sizeof(char)); // start = 0, end = i
+            memcpy(buffer, line + 0, i);
+            printf("alow_buffer: %s\n", buffer);
+            alow = atoi(buffer);
+            free(buffer);
+            break;
+        }
+    }
+
+    for (int i = first_dash + 1; i < strlen(line); i++) { // getting comma gets ahigh
+        if (line[i] == ',') {
+            comma = i;
+            buffer = (char*) calloc(comma - (first_dash + 1), sizeof(char));
+            memcpy(buffer, line + first_dash + 1, i - (first_dash + 1));
+            printf("ahigh_buffer: %s\n", buffer);
+            ahigh = atoi(buffer);
+            free(buffer);
+            break;
+        }
+    }
+
+    for (int i = comma + 1; i < strlen(line); i++) { // getting second dash gets blow
+        if (line[i] == '-') {
+            second_dash = i;
+            buffer = (char*) calloc(second_dash - (comma + 1), sizeof(char));
+            memcpy(buffer, line + comma + 1, i - (comma + 1));
+            printf("blow_buffer: %s\n", buffer);
+            blow = atoi(buffer);
+            free(buffer);
+            break;
+        }
+    }
+
+    // it also gets bhigh
+
+    for (int i = second_dash + 1; i < strlen(line); i++) { // getting second dash gets blow
+        if (line[i] == '\n' || line[i] == 0) {
+            buffer = (char*) calloc(i - (second_dash + 1), sizeof(char));
+            memcpy(buffer, line + second_dash + 1, i - (second_dash + 1));
+            printf("bhigh_buffer: %s\n", buffer);
+            bhigh = atoi(buffer);
+            free(buffer);
+            break;
+        }
+    }
+
+    a->low = alow;
+    a->high = ahigh;
+    b->low = blow;
+    b->high = bhigh;
+}
+
+void part1() {
+    elf_range a;
+    elf_range b;
+    char buffer[32];
+
+    FILE *fp;
+
+    fp = fopen("day4.txt", "r");
+
+    while (!feof) {
+        fgets(buffer, 32, fp);
+
+        get_pair_from_line(buffer, &a, &b);
+    }
+
+    fclose(fp);
+}
+    
+
+int main() {
+    get_pair_from_line("71-87,70-88\n", NULL, NULL);
+
+    part1();
+
+    return 0;
 }
